aa6e9322d0dd3878f61d93d79df60c2d1e4966a0
Alia-eid <146870451+Alia-eid@users.noreply.github.com>
2025-04-26 22:17:41 +0300

    Update InvertedIndex.java

--- src/invertedIndex/InvertedIndex.java
+++ src/invertedIndex/InvertedIndex.java
@@ -1,4 +1,50 @@
 package invertedIndex;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class InvertedIndex {
-}
+
+        private Map<String, Posting> index;
+
+        public InvertedIndex() {
+            index = new HashMap<>();
+        }
+
+        public void addToken(String token, int docId) {
+            if (!index.containsKey(token)) {
+                index.put(token, new Posting(docId));
+            } else {
+                Posting head = index.get(token);
+                Posting current = head;
+                boolean found = false;
+                while (current != null) {
+                    if (current.docId == docId) {
+                        current.dtf++;
+                        found = true;
+                        break;
+                    }
+                    if (current.next == null) break;
+                    current = current.next;
+                }
+                if (!found) {
+                    current.next = new Posting(docId);
+                }
+            }
+        }
+
+        public void printIndex() {
+            for (Map.Entry<String, Posting> entry : index.entrySet()) {
+                System.out.print(entry.getKey() + " -> ");
+                Posting current = entry.getValue();
+                while (current != null) {
+                    System.out.print("(DocID: " + current.docId + ", Freq: " + current.dtf + ") ");
+                    current = current.next;
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+
