c9e198819594e1d96eb72382c8ae7a5ab9ae856b
abdo.mohsen3457643@gmail.com <abdo.mohsen3457643@gmail.com>
2025-04-28 22:42:45 +0200

    fix null pointer exception in query processor file and main test cases

--- src/invertedIndex/Posting.java
+++ src/invertedIndex/Posting.java
@@ -2,21 +2,17 @@ package invertedIndex;
 
 public class Posting {
 
+    Posting(int id) {
+        docId = id;
+    }
     public Posting next = null;
     int docId;
     int dtf = 1;
 
-    Posting(int id, int t) {
-        docId = id;
-        dtf=t;
-    }
     public int getDocID(){
         return docId;
     }
     public int getDTF(){
         return dtf;
-    }
-    Posting(int id) {
-        docId = id;
     }
 }
--- src/queryProcessor/QueryProcessor.java
+++ src/queryProcessor/QueryProcessor.java
@@ -3,11 +3,8 @@ package queryProcessor;
 import java.util.*;
 
 import textProcessor.TextProcessor;
-
 import TfIdfCalculator.AlgoCalculator;
-
-import similarityCalculato.CosineSimilarity;
-
+import similarityCalculator.CosineSimilarity;
 import invertedIndex.*;
 
 public class QueryProcessor {
@@ -20,37 +17,48 @@ public class QueryProcessor {
 
     public List<String> Tokenize()   // normalizing the query
     {
-        Map<String,String>inputText = new HashMap<>();
-        inputText.put("query",query);
-        Map<String,List<String>> tokenizedQuery = TextProcessor.process(inputText);
+        Map<String, String> inputText = new HashMap<>();
+        inputText.put("query", query);
+        Map<String, List<String>> tokenizedQuery = TextProcessor.process(inputText);
         return tokenizedQuery.get("query");
     }
 
-    public void processQuery(String query, InvertedIndex invertedIndex){
+    public void processQuery(String query, InvertedIndex invertedIndex) {
         this.query = query;
         List<String> tokenizedQuery = Tokenize();
 
         Set<String> queryResults = new HashSet<>();
-        Map<Integer,Map<String,Double>> allDocs = AlgoCalculator.calculateTFIDF_allDocuments(); // (DocID,(term,IDF))
-
+        Map<Integer, Map<String, Double>> allDocs = AlgoCalculator.calculateTFIDF_allDocuments(); // (DocID, (term, TF-IDF))
 
         // use CosineSimilarity to calculate similarity between the query and the documents
         CosineSimilarity cosineSimilarity = new CosineSimilarity();
-        // calculate tf-idf
+
+        // calculate tf-idf for query
         Map<String, Double> queryTfIdf = cosineSimilarity.calculateQueryTfIdf(tokenizedQuery);
+
+        // print cosine similarities
         cosineSimilarity.printCosineSimilarity(allDocs, queryTfIdf);
 
-        Map<Integer,Double>finalMap = new HashMap<>();
-        for( String token : tokenizedQuery)
-        {
-            // get docs from the inverted index
+        Map<Integer, Double> finalMap = new HashMap<>();
+
+        for (String token : tokenizedQuery) {
+            // get postings from inverted index
             Posting posting = invertedIndex.getIndex().get(token);
             Posting cur = posting;
-            while(cur!=null){
-                addOrMerge(finalMap,cur.getDocID(),allDocs.get(cur.getDocID()).get(token)); // mapping docs with term idfs
+
+            while (cur != null) {
+                int docId = cur.getDocID();
+                Map<String, Double> docTfIdf = allDocs.get(docId);
+
+                if (docTfIdf != null) {
+                    Double value = docTfIdf.get(token);
+                    if (value != null) {
+                        addOrMerge(finalMap, docId, value); // mapping docs with term tf-idf values
+                    }
+                }
+
                 cur = cur.next;
             }
-
         }
 
         List<Map.Entry<Integer, Double>> sortedList = new ArrayList<>(finalMap.entrySet());
@@ -59,11 +67,10 @@ public class QueryProcessor {
         printResults(sortedList);
     }
 
-    public void printResults(List<Map.Entry<Integer,Double>> results){
+    public void printResults(List<Map.Entry<Integer, Double>> results) {
         for (Map.Entry<Integer, Double> entry : results) {
             System.out.println("Doc" + entry.getKey());
         }
     }
-
 
 }
