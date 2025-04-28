2791ba42f3339b8700d57051f6157073b81b8460
abdo.mohsen3457643@gmail.com <abdo.mohsen3457643@gmail.com>
2025-04-28 23:30:05 +0200

    fix stemming chars bug

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Step 1: Crawl the web
-        System.out.println("Starting the crawler...");
+        // crawl the web
+        System.out.println("start the crawler");
         Crawler crawler = new Crawler();
         crawler.buildCrawler();
         List<String> pages = crawler.getCrawledPages();
         Map<String, String> pageTexts = crawler.getPageTexts();
-        System.out.println("Crawled " + pages.size() + " pages.");
+        System.out.println(pages.size() + " page");
 
-        // Step 2: Preprocess text and build inverted index
-        System.out.println("\nProcessing text and building inverted index...");
+        // preprocess text and build inverted index
+        System.out.println("\nprocessing text and building inverted index");
         InvertedIndex invertedIndex = new InvertedIndex();
         Map<String, List<String>> processedTexts = TextProcessor.process(pageTexts);
 
@@ -33,15 +33,16 @@ public class Main {
             docID++;
         }
 
-        // Step 3: Calculate TF-IDF scores
+        // calculate TF-IDF scores
         System.out.println("\nCalculating TF-IDF...");
         AlgoCalculator algoCalculator = new AlgoCalculator(invertedIndex, crawler);
+//        algoCalculator.printTFIDFVector();
 
-        // Step 4: Print Inverted Index
+        // print Inverted Index
         System.out.println("\nInverted Index:");
         invertedIndex.printIndex();
 
-        // Step 5: Handle user query
+        // andle user query
         System.out.println("\nProcessing Query:");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your search query: ");
